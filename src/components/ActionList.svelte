30cc2507b44c18853e37be214a30a409343fb531
nekename <63245705+nekename@users.noreply.github.com>
2025-06-07 18:42:39 +0100

    refactor: remove ListedAction component

--- src/components/ActionList.svelte
+++ src/components/ActionList.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import type { Action } from "$lib/Action";
 
-	import ListedAction from "./ListedAction.svelte";
-
 	import { localisations } from "$lib/settings";
 	import { invoke } from "@tauri-apps/api/core";
 
@@ -11,16 +9,6 @@
 		categories = await invoke("get_categories");
 	}
 	reload();
-
-	function localiseAction(action: Action, localisations: { [plugin: string]: any } | null): { name: string; tooltip: string } {
-		let { name, tooltip } = { name: action.name, tooltip: action.tooltip };
-		if (localisations && localisations[action.plugin] && localisations[action.plugin][action.uuid]) {
-			let localised = localisations[action.plugin][action.uuid];
-			if (localised.Name) name = localised.Name;
-			if (localised.Tooltip) tooltip = localised.Tooltip;
-		}
-		return { name, tooltip };
-	}
 </script>
 
 <div class="grow mt-1 overflow-auto">
@@ -28,7 +16,16 @@
 		<details open class="mb-2">
 			<summary class="text-xl font-semibold dark:text-neutral-300">{name}</summary>
 			{#each actions as action}
-				<ListedAction {action} localisation={localiseAction(action, $localisations)} />
+				<div class="flex flex-row items-center my-2 space-x-2">
+					<img
+						src={!action.icon.startsWith("opendeck/") ? "http://localhost:57118/" + action.icon : action.icon.replace("opendeck", "")}
+						alt={$localisations?.[action.plugin]?.[action.uuid]?.Tooltip ?? action.tooltip}
+						class="w-12 h-12 rounded-xs"
+						draggable="true"
+						on:dragstart={(event) => event.dataTransfer?.setData("action", JSON.stringify(action))}
+					/>
+					<span class="dark:text-neutral-400">{$localisations?.[action.plugin]?.[action.uuid]?.Name ?? action.name}</span>
+				</div>
 			{/each}
 		</details>
 	{/each}
