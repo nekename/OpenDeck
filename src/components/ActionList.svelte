1c1be55f0df8b6676f6e88f7277efbaca240e4b3
nekename <63245705+nekename@users.noreply.github.com>
2025-08-05 10:26:58 -0600

    Revert "fix: improve dragging actions from the action list (#114)"
    This reverts commit dd47d4316d996af079ace59b483f7a7cc116f222.

--- src/components/ActionList.svelte
+++ src/components/ActionList.svelte
@@ -12,21 +12,18 @@
 	reload();
 </script>
 
-<div class="grow mt-1 overflow-auto select-none">
+<div class="grow mt-1 overflow-auto">
 	{#each Object.entries(categories).sort((a, b) => a[0] == "OpenDeck" ? -1 : b[0] == "OpenDeck" ? 1 : a[0].localeCompare(b[0])) as [name, actions]}
 		<details open class="mb-2">
 			<summary class="text-xl font-semibold dark:text-neutral-300">{name}</summary>
 			{#each actions as action}
-				<div
-					class="flex flex-row items-center my-2 space-x-2"
-					role="group"
-					draggable="true"
-					on:dragstart={(event) => event.dataTransfer?.setData("action", JSON.stringify(action))}
-				>
+				<div class="flex flex-row items-center my-2 space-x-2">
 					<img
 						src={!action.icon.startsWith("opendeck/") ? "http://localhost:57118/" + action.icon : action.icon.replace("opendeck", "")}
 						alt={$localisations?.[action.plugin]?.[action.uuid]?.Tooltip ?? action.tooltip}
 						class="w-12 h-12 rounded-xs"
+						draggable="true"
+						on:dragstart={(event) => event.dataTransfer?.setData("action", JSON.stringify(action))}
 					/>
 					<span class="dark:text-neutral-400">{$localisations?.[action.plugin]?.[action.uuid]?.Name ?? action.name}</span>
 				</div>
