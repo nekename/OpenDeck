dba5277419a56373da102eb5f285536015f1b28c
nekename <63245705+nekename@users.noreply.github.com>
2025-08-11 19:46:27 +0100

    feat: enable libei support for Input Simulation

--- plugins/com.amansprojects.starterpack.sdPlugin/src/input_simulation.rs
+++ plugins/com.amansprojects.starterpack.sdPlugin/src/input_simulation.rs
@@ -12,19 +12,24 @@ static ENIGO: LazyLock<Mutex<Option<Enigo>>> = LazyLock::new(|| Mutex::new(Optio
 
 pub async fn down_up(event: KeyEvent, action: &str) -> EventHandlerResult {
 	if let Some(value) = event.payload.settings.as_object().unwrap().get(action) {
-		let value = value.as_str().unwrap();
+		let value = value.as_str().unwrap().to_owned();
 		if value.trim().is_empty() {
 			return Ok(());
 		}
 		let mut enigo_guard = ENIGO.lock().await;
-		if enigo_guard.is_none() {
-			enigo_guard.replace(Enigo::new(&Settings::default())?);
-		}
-		let enigo = enigo_guard.as_mut().unwrap();
-		let tokens: Vec<Token> = ron::from_str(value)?;
-		for token in tokens {
-			enigo.execute(&token).unwrap();
-		}
+		std::thread::spawn(move || -> EventHandlerResult {
+			if enigo_guard.is_none() {
+				enigo_guard.replace(Enigo::new(&Settings::default())?);
+			}
+			let enigo = enigo_guard.as_mut().unwrap();
+			let tokens: Vec<Token> = ron::from_str(&value)?;
+			for token in tokens {
+				enigo.execute(&token).unwrap();
+			}
+			Ok(())
+		})
+		.join()
+		.unwrap()?;
 	}
 
 	Ok(())
