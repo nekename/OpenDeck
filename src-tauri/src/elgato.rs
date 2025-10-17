b488ede2c35116b6f00ee35ea69a3da51f2da36f
nekename <63245705+nekename@users.noreply.github.com>
2025-10-17 19:01:53 +0100

    feat: update elgato-streamdeck to support Stream Deck Modules

--- src-tauri/src/elgato.rs
+++ src-tauri/src/elgato.rs
@@ -75,9 +75,9 @@ async fn init(device: AsyncStreamDeck, device_id: String) {
 
 	let kind = device.kind();
 	let device_type = match kind {
-		Kind::Original | Kind::OriginalV2 | Kind::Mk2 | Kind::Mk2Scissor => 0,
-		Kind::Mini | Kind::MiniMk2 => 1,
-		Kind::Xl | Kind::XlV2 => 2,
+		Kind::Original | Kind::OriginalV2 | Kind::Mk2 | Kind::Mk2Scissor | Kind::Mk2Module => 0,
+		Kind::Mini | Kind::MiniMk2 | Kind::MiniMk2Module => 1,
+		Kind::Xl | Kind::XlV2 | Kind::XlV2Module => 2,
 		Kind::Pedal => 5,
 		Kind::Plus => 7,
 		Kind::Neo => 9,
