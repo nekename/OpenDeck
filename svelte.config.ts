11a06332c7681b98cf2b29bce0828c6e504342dd
nekename <63245705+nekename@users.noreply.github.com>
2026-02-18 11:01:08 +0000

    refactor: replace svelte.config.js with svelte.config.ts to eliminate untyped code

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,13 +1,11 @@
+import type { Config } from "@sveltejs/kit";
+
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-/** @type {import("@sveltejs/kit").Config} */
-const config = {
+export default {
 	preprocess: vitePreprocess(),
-
 	kit: {
 		adapter: adapter(),
 	},
-};
-
-export default config;
+} satisfies Config;
