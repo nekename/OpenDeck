c88c6eb694c5f53d09ab2226dfdc8698c416e04d
Craig McLure <craig@mclure.net>
2026-06-14 13:47:03 +0100

    imports: consolidate crate imports

--- src-tauri/src/events/inbound/states.rs
+++ src-tauri/src/events/inbound/states.rs
@@ -1,9 +1,10 @@
 use super::ContextAndPayloadEvent;
 use crate::events::frontend::instances::update_state;
+use crate::shared::{config_dir, load_encoder_layout};
 use crate::store::profiles::{acquire_locks_mut, debounce_profile_save, get_instance_mut, save_profile};
+
 use log::{debug, warn};
 
-use crate::shared::{config_dir, load_encoder_layout};
 use serde::Deserialize;
 use serde_json::Value;
 
